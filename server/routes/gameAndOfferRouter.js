4ccfa5615c8d8a16ed05c0586c947693340f22d5
Max Kurchiy <Kradsuzmax@gmail.com>
2023-03-13 11:42:33 +0300

    route model game

--- server/routes/gameAndOfferRouter.js
+++ server/routes/gameAndOfferRouter.js
@@ -5,8 +5,24 @@ const { GamePlatform, Game, Offer } = require("../db/models");
 const gameAndOfferRouter = express.Router();
 
 gameAndOfferRouter.post("/add", async (req, res) => {
-  const game = await req.body;
-  console.log(game);
+  const offer = await req.body.data;
+  const game = await req.body.game;
+  console.log("GGGAAAAAMMMEEE", game);
+  const createGame = await Game.findOrCreate({
+    where: { name: game.name },
+    defaults: {
+      cover: game.cover.image_id,
+      date: game.first_release_date,
+      genres: game.genres?.map((el) => el.name).join(", "),
+      name: game.name,
+      rating: game.rating,
+      summaru: game.summary,
+      apiGameId: game.id,
+    },
+  });
+  console.log("OFFER", offer);
+  //   console.log("GAME", game);
+  //   console.log(new Date(game.first_release_date).getUTCFullYear());
 });
 
 module.exports = gameAndOfferRouter;
