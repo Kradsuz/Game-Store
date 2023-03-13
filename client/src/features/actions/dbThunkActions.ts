26ad0042f9f5e27e31a5b993133dfc2d994bf81e
Max Kurchiy <Kradsuzmax@gmail.com>
2023-03-13 15:24:41 +0300

    all routes

--- client/src/features/actions/dbThunkActions.ts
+++ client/src/features/actions/dbThunkActions.ts
@@ -4,9 +4,9 @@ import type { GameType } from '../../types';
 
 export const getOffersThunkAction = createAsyncThunk<number>(
   'offers/fetch',
-  async (id:number) => {
+  async (id: number) => {
     try {
-      const response = await axios.post('/api/games/offers', (id));
+      const response = await axios.post('/api/games/offers', { id });
       return response.data;
     } catch (error) {
       console.error(error);
@@ -16,14 +16,14 @@ export const getOffersThunkAction = createAsyncThunk<number>(
 );
 
 export const getDBGamesThunkAction = createAsyncThunk<GameType[], string>(
-    'games/fetch',
-    async () => {
-      try {
-        const response = await axios.post<GameType[]>('/api/games');
-        return response.data;
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-    },
-  );
+  'games/fetch',
+  async () => {
+    try {
+      const response = await axios.post<GameType[]>('/api/games');
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+);
