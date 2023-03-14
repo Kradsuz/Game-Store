a57a5e0b88ab6c2236096e68fe0997d8767424b5
Alexander Elkin <alexelkin17@yandex.ru>
2023-03-14 18:35:01 +0300

    mainpage cards

--- client/src/components/Pages/LKSeller/index.tsx
+++ client/src/components/Pages/LKSeller/index.tsx
@@ -1,40 +1,41 @@
-import {
-  Card,
-  CardContent,
-  CardMedia,
-  Grid,
-  Link,
-  Typography,
-} from '@mui/material';
+export {}
+// import {
+//   Card,
+//   CardContent,
+//   CardMedia,
+//   Grid,
+//   Link,
+//   Typography,
+// } from '@mui/material';
 
-import React from 'react';
-import type { GameType } from '../../../types';
+// import React from 'react';
+// import type { GameType } from '../../../types';
 
-type OneOfferProps = {
-  allOffersSeller: GameType;
-};
+// type OneOfferProps = {
+//   allOffersSeller: GameType;
+// };
 
-export default function index({ allOffersSeller }: OneOfferProps): JSX.Element {
-  return (
-    <Grid item xs={3} sm={3} md={3} lg={3} sx={{ height: 400 }}>
-      <Card sx={{ height: '100%' }}>
-        <Link to={`/games/${game.id}`}>
-          <CardMedia
-            component="img"
-            alt="gameImg"
-            height="200"
-            image={`https://images.igdb.com/igdb/image/upload/t_screenshot_med/${game?.cover?.image_id}.jpg`}
-          />
-        </Link>
-        <CardContent className="d-flex flex-column">
-          <Typography gutterBottom variant="h6" component="div">
-            {game.name}
-          </Typography>
-          {game?.platforms?.map((platform: PlatformsType) => (
-            <Typography key={platform.id}>{platform.abbreviation}</Typography>
-          ))}
-        </CardContent>
-      </Card>
-    </Grid>
-  );
-}
+// export default function index({ allOffersSeller }: OneOfferProps): JSX.Element {
+//   return (
+//     <Grid item xs={3} sm={3} md={3} lg={3} sx={{ height: 400 }}>
+//       <Card sx={{ height: '100%' }}>
+//         <Link to={`/games/${game.id}`}>
+//           <CardMedia
+//             component="img"
+//             alt="gameImg"
+//             height="200"
+//             image={`https://images.igdb.com/igdb/image/upload/t_screenshot_med/${game?.cover?.image_id}.jpg`}
+//           />
+//         </Link>
+//         <CardContent className="d-flex flex-column">
+//           <Typography gutterBottom variant="h6" component="div">
+//             {game.name}
+//           </Typography>
+//           {game?.platforms?.map((platform: PlatformsType) => (
+//             <Typography key={platform.id}>{platform.abbreviation}</Typography>
+//           ))}
+//         </CardContent>
+//       </Card>
+//     </Grid>
+//   );
+// }
