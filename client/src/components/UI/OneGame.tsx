ffd78c996f4adbd2d80e6fa6916d7aa7e285a434
Alexander Elkin <alexelkin17@yandex.ru>
2023-03-14 09:55:26 +0300

    game offers from sellers

--- client/src/components/UI/OneGame.tsx
+++ client/src/components/UI/OneGame.tsx
@@ -13,11 +13,11 @@ type OneGameProps = {
 
 function OneGame({ game }: OneGameProps): JSX.Element {
   console.log(game.platforms, '<======');
-  
+
   return (
     <Grid item xs={12} sm={6} md={4} sx={{ height: 400 }}>
       <Card sx={{ height: '100%' }}>
-        <Link to={`/offers/${game.id}`}>
+        <Link to={`/sellers/${game.id}`}>
           <CardMedia
             component="img"
             alt="gameImg"
@@ -29,11 +29,9 @@ function OneGame({ game }: OneGameProps): JSX.Element {
           <Typography gutterBottom variant="h6" component="div">
             {game.name}
           </Typography>
-          {game?.platforms?.map(
-            (platform: PlatformsType) => (
-              <Typography key={platform.id}>{platform.abbreviation}</Typography>
-            ),
-          )}
+          {game?.platforms?.map((platform: PlatformsType) => (
+            <Typography key={platform.id}>{platform.abbreviation}</Typography>
+          ))}
         </CardContent>
       </Card>
     </Grid>
--- client/src/features/actions/dbThunkActions.ts
+++ client/src/features/actions/dbThunkActions.ts
@@ -3,12 +3,14 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import type { DbGameType } from '../../types';
 
 export const getOffersThunkAction = createAsyncThunk<DbGameType, number>(
-  'offers/fetch',
+  'sellers/fetch',
   async (id: number) => {
     try {
-      const response = await axios.post<DbGameType>('/api/games/offers', {id});
+      const response = await axios.post<DbGameType>('/api/games/sellers', {
+        id,
+      });
       console.log(response.data);
-      
+
       return response.data;
     } catch (error) {
       console.error(error);
