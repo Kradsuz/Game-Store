ee44b9715671d340bf4fa2730cfa817f9a9355cd
Max Kurchiy <Kradsuzmax@gmail.com>
2023-03-14 14:48:39 +0300

    type LK

--- client/src/components/Pages/LKSeller/index.tsx
+++ client/src/components/Pages/LKSeller/index.tsx
@@ -11,24 +11,24 @@ import React from 'react';
 import type { GameType } from '../../../types';
 
 type OneOfferProps = {
-  allOffersSeller: GameType;
+  offersSeller: GameType;
 };
 
-export default function index({ allOffersSeller }: OneOfferProps): JSX.Element {
+export default function index({ offersSeller }: OneOfferProps): JSX.Element {
   return (
     <Grid item xs={3} sm={3} md={3} lg={3} sx={{ height: 400 }}>
       <Card sx={{ height: '100%' }}>
-        <Link to={`/games/${game.id}`}>
+        <Link to={`/games/${offersSeller.id}`}>
           <CardMedia
             component="img"
             alt="gameImg"
             height="200"
-            image={`https://images.igdb.com/igdb/image/upload/t_screenshot_med/${game?.cover?.image_id}.jpg`}
+            // image={`https://images.igdb.com/igdb/image/upload/t_screenshot_med/${game?.cover?.image_id}.jpg`}
           />
         </Link>
         <CardContent className="d-flex flex-column">
           <Typography gutterBottom variant="h6" component="div">
-            {game.name}
+            {offersSeller.name}
           </Typography>
           {game?.platforms?.map((platform: PlatformsType) => (
             <Typography key={platform.id}>{platform.abbreviation}</Typography>
